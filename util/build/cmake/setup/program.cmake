fe8e249401c3e4ee2d2936d66afbbb24ab014077
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-02-10 23:44:59 -0500

    Make Clang-Tidy integration optional.
    This commit also removes unnecessary search for the Valgrind
    executable.

--- util/build/cmake/setup/program.cmake
+++ util/build/cmake/setup/program.cmake
@@ -1,17 +1,10 @@
 find_program(CLANG_TIDY_EXE NAMES "clang-tidy" DOC "Path to clang-tidy static analysis tool.")
 if (NOT CLANG_TIDY_EXE)
     message(WARNING 
-        "Clang-tidy executable not found on system - linting will be disabled for for Makefile and Ninja generators."
+        "Clang-tidy executable not found on system - linting will not be available."
     )
 else()
-    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} -p ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json)
-endif()
-
-
-# Find Valgrind for use with CTest.
-find_program(VALGRIND_EXE NAMES "valgrind" DOC "Path to valgrind dynamic analysis tool.")
-if (NOT VALGRIND_EXE)
-    message(WARNING 
-        "Valgrind executable not found on system - Valgrind CMake configuration will not be usable."
-    )
+    if(STRICT_CLANG_TIDY)
+        set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} --warnings-as-errors=* -p ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json)
+    endif()
 endif()
