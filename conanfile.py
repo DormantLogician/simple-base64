6e62be95473ae6dfb56ba43f61c6545453518c7f
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-09-07 14:51:56 -0400

    Migrate to Conan 2.0.
    This upgrades the Conan-CMake integration and changes 'target'
    folder structure slightly so it is more flat.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,7 @@
-from conans import ConanFile, CMake
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake
 
 class ConfigConan(ConanFile):
     name = "sb64"
@@ -6,21 +9,40 @@ class ConfigConan(ConanFile):
     author = "Stephen Aaron Hannon <hannonstephen19@gmail.com>"
     license = "MIT"
     description = """Simple base 64 encoder/decoder library."""
-
-    generators = "cmake_find_package"
-    settings = "os", "compiler", "build_type", "arch"
-    tool_requires = "boost/1.81.0"
     upload_policy = "skip"
 
-    exports_sources = "CMakeLists.txt", "CMakePresets.json", "LICENSE.txt", "target/*", "util/*"
+    requires = "boost/1.83.0"
+    generators = "CMakeDeps"
+
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    exports_sources = "CMakeLists.txt", "CMakePresets.json", "LICENSE.txt", ".clang-tidy", "target/*", "util/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
+
+    def layout(self):
+        self.folders.build = os.path.join("built", "single")
+        self.folders.generators = os.path.join(self.folders.build, "generators")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-        cmake.test()
+        self.run("cmake --preset release", cwd=self.source_folder)
+        self.run("cmake --build --preset release", cwd=self.source_folder)
 
     def package(self):
+        self.run("ctest --preset release", cwd=self.source_folder)
         cmake = CMake(self)
         cmake.install()
 
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,30 @@
 import os
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.build import can_run
 
 class ConfigTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self):
-          os.chdir("bin")
-          self.run('.%ssb64_conan_package_test' % os.sep)
+        if can_run(self):
+          cmd = os.path.join(self.cpp.build.bindir, "sb64_conan_package_test")
+          self.run(cmd, env="conanrun")
