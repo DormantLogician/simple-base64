129da6ab018fb29c3b6b3af845acbc748572bcdd
Stephen Aaron Hannon <hannonstephen19@gmail.com>
2023-09-08 20:18:46 -0400

    Add Debian-style SONAME property to library target.
    This changes the package name of the library target
    and makes it more compliant with Debian Linux conventions
    where applicable.

--- util/build/cmake/setup/packing.cmake
+++ util/build/cmake/setup/packing.cmake
@@ -6,6 +6,10 @@ set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE.txt)
 set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/dist)
 set(CPACK_THREADS 2)
 
+set(PROJECT_DEBIAN_SOVERSION 1)
+set(CPACK_PACKAGE_FILE_NAME libsb64-${PROJECT_DEBIAN_SOVERSION})
+set(CPACK_SOURCE_PACKAGE_FILE_NAME ${CPACK_PACKAGE_FILE_NAME})
+
 set(CPACK_PACKAGE_CONTACT "Stephen Aaron Hannon <hannonstephen19@gmail.com>")
 
 set(CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS # Make sure directory permissions are set properly.
@@ -17,6 +21,9 @@ set(CPACK_INSTALL_DEFAULT_DIRECTORY_PERMISSIONS # Make sure directory permission
 set(CPACK_SOURCE_GENERATOR ZIP)
 set(CPACK_SOURCE_IGNORE_FILES /built;/test_package/build;/.git;/.vscode;/CMakeUserPresets.json)
 
-set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT) # Use Debian naming conventions for binary files.
 set(CPACK_DEBIAN_PACKAGE_SECTION libdevel)
-set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES) # Make better dependency list in deb file.
+set(CPACK_DEBIAN_PACKAGE_NAME ${CPACK_PACKAGE_FILE_NAME})
+
+# Make better dependency list in deb file.
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES)
+set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS YES)
